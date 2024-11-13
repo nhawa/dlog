1c56b80f34b98668f2318094a452c466cffdf3a0
anwar sandi <anwar.sandi@ottencoffee.co.id>
2024-11-13 23:50:24 +0700

    update structure log

--- main.go
+++ main.go
@@ -3,36 +3,19 @@ package dlog
 import (
 	"encoding/json"
 	"fmt"
-	"reflect"
 	"runtime"
 
 	"github.com/sirupsen/logrus"
 )
 
-func PrintStructFields(x ...interface{}) {
-	for _, s := range x {
-		v := reflect.ValueOf(s)
-		t := reflect.TypeOf(s)
-
-		fmt.Println("=======================================================================================================")
-		fmt.Printf("Fields of struct %s:\n", t.Name())
-		fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-		for i := 0; i < v.NumField(); i++ {
-			field := v.Field(i)
-			fieldName := t.Field(i).Name
-			fieldValue := field.Interface()
-
-			fmt.Printf("%s\t: %+v\n", fieldName, fieldValue)
-		}
-		fmt.Println("=======================================================================================================")
-
-	}
-}
-
 func LogJson(x ...interface{}) {
+	var fields []interface{}
+
 	_, fn, line, _ := runtime.Caller(1)
+	fields = append(fields, fmt.Sprintf("log on: %s:%d |", fn, line))
 	for _, data := range x {
 		d, _ := json.Marshal(data)
-		logrus.Info(fmt.Sprintf("[error] %s:%d", fn, line), string(d))
+		fields = append(fields, fmt.Sprintf(" %s |", string(d)))
 	}
+	logrus.Info(fields...)
 }
